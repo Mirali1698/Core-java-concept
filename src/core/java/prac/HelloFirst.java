cba4e9476fa9661b171f1a5176dda2d475e1cc0d
Mirali Talaviya <miralitalaviya123@gmail.com>
2023-07-09 23:23:12 -0400

    Add HelloFirst file

---
+++ src/core/java/prac/HelloFirst.java
@@ -0,0 +1,10 @@
+package core.java.prac;
+
+public class HelloFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Helloworld class");
+	}
+
+}
